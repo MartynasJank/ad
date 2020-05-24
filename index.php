bd71f3544d4f77bab9605e84d300ecb84b9bf8e9
Martynas <martynasjank@gmail.com>
2020-05-24 14:37:30 +0300

    upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<script type="text/javascript" src="//lv.adocean.pl/files/js/aomini.js"></script>
+<div data-ao-pid="ndSR0nOwbG62AyewMOm_Vhna4mmbVt6LA_pd5_sYYsn.O7" style="display: none;"></div>
+<script type="text/javascript">
+    // AdOcean 2003-2019
+    /* PLACEMENT: Setup.jurbarkosviesa.lt.jurbarkosviesa.lt_300x250_1 */
+    (window['aomini'] || (window['aomini'] === [])).push({
+        cmd: 'display',
+        server: 'lv.adocean.pl',
+        id: "ndSR0nOwbG62AyewMOm_Vhna4mmbVt6LA_pd5_sYYsn.O7",
+        consent: null
+    });
+</script>
